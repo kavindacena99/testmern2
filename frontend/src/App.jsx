e7192d354face8e9d281ea9fa9a6a44db64b5817
Kavinda Supun Dissanayake <102888367+kavindacena99@users.noreply.github.com>
2025-03-25 03:10:30 +0000

    test5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,7 @@
-import React from "react";
-
 function App(){
-  const getUser = () => {
-    fetch("/api/user")
-    .then(res => res.json())
-    .then(json => console.log(json))
-  }
-
-  useEffect(()=>{
-    getUser();
-  },[]);
-
   return(
     <div>
-      <h1>ccue</h1>
+      <h1>Hello</h1>
     </div>
   );
 }
